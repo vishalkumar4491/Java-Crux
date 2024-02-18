bfe430d58b5f9159438360a86a572e5bd429549c
vishalkumar4491 <vishalkumar.05423@gmail.com>
2024-02-18 15:39:30 +0530

    Conditional Statements

---
+++ src/O1_Basics/O3_ConditionalStatement.java
@@ -0,0 +1,15 @@
+package O1_Basics;
+
+public class O3_ConditionalStatement {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int a = 30;
+        int b = 20;
+
+        if(a < b){
+            System.out.println("in if");
+        }else{
+            System.out.println("in else");
+        }
+    }
+}
