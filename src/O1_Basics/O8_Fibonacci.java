f58efbce55bd1108836cdcbc877b13e4cb59b73a
vishalkumar4491 <vishalkumar.05423@gmail.com>
2024-02-18 16:34:47 +0530

    Fibonacci

---
+++ src/O1_Basics/O8_Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class O8_Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int n1 = 0, n2 = 1;
+        int i = 1;
+        while(i <= n){
+            System.out.println(n2);
+            int x = n1;
+            n1 = n2;
+            n2 += x;
+            i += 1;
+        }
+    }
+}
